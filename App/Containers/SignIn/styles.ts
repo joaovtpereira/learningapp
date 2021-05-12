70be1ef7f75c68457151b194fa993815c5cbc64b
joaovtpereira <joaovitorpessoal2@gmail.com>
2021-05-12 14:20:53 -0300

    fix: exclusao de estilizações nao usadas

--- App/Containers/SignIn/styles.ts
+++ App/Containers/SignIn/styles.ts
@@ -20,21 +20,10 @@ export const FormSignin = styled.View`
   height: ${`${String((Dimensions.get('screen').height / 4) * 3)}px`};
 `;
 
-export const FormContent = styled.View`
-  flex: 1;
-  width: 100%;
-`;
-
 export const ButtonContainer = styled.View`
   width: 100%;
   align-items: center;
   padding: 0 40px;
 
   margin-bottom: ${`${String(Dimensions.get('screen').height / 4)}px`};
-`;
-
-export const Logo = styled.View`
-  width: 100%;
-  align-items: center;
-  margin-top: ${`${String(Dimensions.get('screen').height / 4)}px`};
 `;
